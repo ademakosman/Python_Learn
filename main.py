b0d86bb91f52bbff849ccffd39f48a328ad7182d
ADEM <akosman.adem@gmail.com>
2021-12-05 21:46:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#Data Types
+# print(len(input("What is your name\n")))
+
+# str komutu intergerı stringe çevirir
+# num_char = len(input("What is your name\n"))
+# new_num_char = str(num_char)
+# print(type(new_num_char))  ! new_num_char değişken türünü verir
+# print("your name has " + new_num_char + "Characters.")
+
+
