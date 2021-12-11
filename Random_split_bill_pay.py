348516f77509dc1a1f99bd468e1df37c0f7d09a1
ADEM <akosman.adem@gmail.com>
2021-12-11 13:10:37 +0000

    update

--- Random_split_bill_pay.py
+++ Random_split_bill_pay.py
@@ -6,7 +6,7 @@ names = names_string.split(", ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-random_person = random.randint(0,len(names))
+random_person = random.randint(0,len(names)-1)
 print(names)
 print("random number", random_person)
 print("lucky person " + names[random_person] + " will pay")
